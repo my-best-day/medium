c0f6eb0e620adfa7219ea0d10ae2625fa462ca93
Ron Hirsch <ronhirsch@gmail.com>
2023-11-30 22:07:18 -0500

    dropped NSP. Early stages of EarlyStopping

--- article.py
+++ article.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import torch
 import re
 import random
-import transformers, datasets
 from tokenizers import BertWordPieceTokenizer
 from transformers import BertTokenizer
 import tqdm
@@ -15,61 +14,46 @@ import numpy as np
 from torch.optim import Adam
 
 PREPARE_DATA = False
-MAX_LEN = 64
+MAX_LEN = 32 # 64
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 ### loading all data into memory
-corpus_movie_conv = './datasets/movie_conversations.txt'
 corpus_movie_lines = './datasets/movie_lines.txt'
-with open(corpus_movie_conv, 'r', encoding='iso-8859-1') as c:
-    conv = c.readlines()
+
+# lineId, characterId, movieId, character name, text
 with open(corpus_movie_lines, 'r', encoding='iso-8859-1') as l:
-    lines = l.readlines()
+    records = l.readlines()
 
 ### splitting text using special lines
-lines_dic = {}
-for line in lines:
-    objects = line.split(" +++$+++ ")
-    lines_dic[objects[0]] = objects[-1]
+# lineId -> text
+lines = []
+for record in records:
+    objects = record.split(" +++$+++ ")
+    line = objects[-1]
+    lines.append(line)
 
-### generate question answer pairs
-pairs = []
-for con in conv:
-    ids = eval(con.split(" +++$+++ ")[-1])
-    for i in range(len(ids)):
-        qa_pairs = []
-        
-        if i == len(ids) - 1:
-            break
-
-        first = lines_dic[ids[i]].strip()  
-        second = lines_dic[ids[i+1]].strip() 
-        qa_pairs.append(' '.join(first.split()[:MAX_LEN]))
-
-        qa_pairs.append(' '.join(second.split()[:MAX_LEN]))
-        pairs.append(qa_pairs)
-
+# truncate long sentences
+lines = [' '.join(line.split()[:MAX_LEN]) for line in lines]
 
 if PREPARE_DATA:
     # WordPiece tokenizer
 
     ### save data as txt file
     os.mkdir('./data')
-    text_data = []
+
+    content = []
     file_count = 0
 
-    for sample in tqdm.tqdm([x[0] for x in pairs]):
-        text_data.append(sample)
+    for line in tqdm.tqdm(lines):
+        content.append(line)
 
         # once we hit the 10K mark, save to file
-        if len(text_data) == 10000:
+        if len(content) == 10000:
             with open(f'./data/text_{file_count}.txt', 'w', encoding='utf-8') as fp:
-                fp.write('\n'.join(text_data))
-            text_data = []
+                fp.write('\n'.join(content))
+            content = []
             file_count += 1
-
-
 
     paths = [str(x) for x in Path('./data').glob('**/*.txt')]
 
@@ -95,47 +79,75 @@ if PREPARE_DATA:
 
 tokenizer = BertTokenizer.from_pretrained('./bert-it-1/bert-it-vocab.txt', local_files_only=True)
 
+class EarlyStopping:
+    def __init__(self, model, path, patience, verbose, delta):
+        self.model = model
+        self.path = path
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_loss = None
+        self.delta = delta
+        self.stop_flag = False
 
+    def __call__(self, loss):
+        if self.best_loss is None:
+            self.best_loss = loss
+        elif loss >= self.best_loss + self.delta:
+            self.counter += 1
+            print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                print(f"Early Stopping, loss doesn't improves. Current: {loss}, Best: {self.best_loss}")
+                self.stop_flag = True
+        elif loss < self.best_loss - self.delta:
+            self.best_loss = loss
+            self.counter = 0
+            self.save_checkpoint(loss)
+
+    def save_checkpoint(self, loss):
+        if self.verbose:
+            print(f'Loss decreased {self.best_loss} --> {loss}. Saving model ...')
+        state = {
+            'model': self.model.state_dict(),
+            'loss': loss,
+            'counter': self.counter
+        }
+        torch.save(state,  self.path + '/' + 'checkpoint.pth')
+       
 
 class BERTDataset(Dataset):
-    def __init__(self, data_pair, tokenizer, seq_len=64):
+    def __init__(self, lines, tokenizer, seq_len=64):
 
         self.tokenizer = tokenizer
         self.seq_len = seq_len
-        self.corpus_lines = len(data_pair)
-        self.lines = data_pair
+        self.corpus_lines = len(lines)
+        self.lines = lines
 
     def __len__(self):
         return self.corpus_lines
 
-    def __getitem__(self, item):
+    def __getitem__(self, index):
 
         # Step 1: get random sentence pair, either negative or positive (saved as is_next_label)
-        t1, t2, is_next_label = self.get_sent(item)
+        line = lines[index] 
 
         # Step 2: replace random words in sentence with mask / random words
-        t1_random, t1_label = self.random_word(t1)
-        t2_random, t2_label = self.random_word(t2)
+        sentence, label = self.random_word(line)
 
         # Step 3: Adding CLS and SEP tokens to the start and end of sentences
          # Adding PAD token for labels
-        t1 = [self.tokenizer.vocab['[CLS]']] + t1_random + [self.tokenizer.vocab['[SEP]']]
-        t2 = t2_random + [self.tokenizer.vocab['[SEP]']]
-        t1_label = [self.tokenizer.vocab['[PAD]']] + t1_label + [self.tokenizer.vocab['[PAD]']]
-        t2_label = t2_label + [self.tokenizer.vocab['[PAD]']]
+        t1 = [self.tokenizer.vocab['[CLS]']] + sentence + [self.tokenizer.vocab['[SEP]']]
+        t1_label = [self.tokenizer.vocab['[PAD]']] + label + [self.tokenizer.vocab['[PAD]']]
 
         # Step 4: combine sentence 1 and 2 as one input
         # adding PAD tokens to make the sentence same length as seq_len
-        segment_label = ([1 for _ in range(len(t1))] + [2 for _ in range(len(t2))])[:self.seq_len]
-        bert_input = (t1 + t2)[:self.seq_len]
-        bert_label = (t1_label + t2_label)[:self.seq_len]
+        bert_input = t1[:self.seq_len]
+        bert_label = t1_label[:self.seq_len]
         padding = [self.tokenizer.vocab['[PAD]'] for _ in range(self.seq_len - len(bert_input))]
-        bert_input.extend(padding), bert_label.extend(padding), segment_label.extend(padding)
+        bert_input.extend(padding), bert_label.extend(padding), label.extend(padding)
 
         output = {"bert_input": bert_input,
-                  "bert_label": bert_label,
-                  "segment_label": segment_label,
-                  "is_next": is_next_label}
+                  "bert_label": bert_label}
 
         return {key: torch.tensor(value) for key, value in output.items()}
 
@@ -181,29 +193,10 @@ class BERTDataset(Dataset):
         assert len(output) == len(output_label)
         return output, output_label
 
-    def get_sent(self, index):
-        '''return random sentence pair'''
-        t1, t2 = self.get_corpus_line(index)
-
-        # negative or positive pair, for next sentence prediction
-        if random.random() > 0.5:
-            return t1, t2, 1
-        else:
-            return t1, self.get_random_line(), 0
-
-    def get_corpus_line(self, item):
-        '''return sentence pair'''
-        return self.lines[item][0], self.lines[item][1]
-
-    def get_random_line(self):
-        '''return random single sentence'''
-        return self.lines[random.randrange(len(self.lines))][1]
-
-
 
 class PositionalEmbedding(torch.nn.Module):
 
-    def __init__(self, d_model, max_len=128):
+    def __init__(self, d_model, max_len):
         super().__init__()
 
         # Compute the positional encodings once in log space.
@@ -217,8 +210,8 @@ class PositionalEmbedding(torch.nn.Module):
                 pe[pos, i + 1] = math.cos(pos / (10000 ** ((2 * (i + 1))/d_model)))
 
         # include the batch size
-        self.pe = pe.unsqueeze(0)   
-        # self.register_buffer('pe', pe)
+        # self.pe = pe.unsqueeze(0)   
+        self.register_buffer('pe', pe.unsqueeze(0))
 
     def forward(self, x):
         return self.pe
@@ -228,11 +221,10 @@ class BERTEmbedding(torch.nn.Module):
     BERT Embedding which is consisted with under features
         1. TokenEmbedding : normal embedding matrix
         2. PositionalEmbedding : adding positional information using sin, cos
-        2. SegmentEmbedding : adding sentence segment info, (sent_A:1, sent_B:2)
         sum of all these features are output of BERTEmbedding
     """
 
-    def __init__(self, vocab_size, embed_size, seq_len=64, dropout=0.1):
+    def __init__(self, vocab_size, embed_size, seq_len, dropout):
         """
         :param vocab_size: total vocab size
         :param embed_size: embedding size of token embedding
@@ -244,12 +236,12 @@ class BERTEmbedding(torch.nn.Module):
         # (m, seq_len) --> (m, seq_len, embed_size)
         # padding_idx is not updated during training, remains as fixed pad (0)
         self.token = torch.nn.Embedding(vocab_size, embed_size, padding_idx=0)
-        self.segment = torch.nn.Embedding(3, embed_size, padding_idx=0)
         self.position = PositionalEmbedding(d_model=embed_size, max_len=seq_len)
         self.dropout = torch.nn.Dropout(p=dropout)
        
-    def forward(self, sequence, segment_label):
-        x = self.token(sequence) + self.position(sequence) + self.segment(segment_label)
+    def forward(self, sequence):
+        # print("*** *** *** *** DEVICES: ", sequence.device)
+        x = self.token(sequence) + self.position(sequence)
         return self.dropout(x)
 
 
@@ -322,13 +314,8 @@ class FeedForward(torch.nn.Module):
         out = self.fc2(self.dropout(out))
         return out
 class EncoderLayer(torch.nn.Module):
-    def __init__(
-        self, 
-        d_model=768,
-        heads=12, 
-        feed_forward_hidden=768 * 4, 
-        dropout=0.1
-        ):
+    # def __init__(self, d_model=768, heads=12, feed_forward_hidden=768 * 4, dropout=0.1):
+    def __init__(self, d_model, heads, feed_forward_hidden, dropout):
         super(EncoderLayer, self).__init__()
         self.layernorm = torch.nn.LayerNorm(d_model)
         self.self_multihead = MultiHeadedAttention(heads, d_model)
@@ -352,7 +339,8 @@ class BERT(torch.nn.Module):
     BERT model : Bidirectional Encoder Representations from Transformers.
     """
 
-    def __init__(self, vocab_size, d_model=768, n_layers=12, heads=12, dropout=0.1):
+    # def __init__(self, vocab_size, d_model=768, n_layers=12, heads=12, dropout=0.1):
+    def __init__(self, vocab_size, d_model, n_layers, heads, dropout):
         """
         :param vocab_size: vocab_size of total words
         :param hidden: BERT model hidden size
@@ -369,20 +357,20 @@ class BERT(torch.nn.Module):
         # paper noted they used 4 * hidden_size for ff_network_hidden_size
         self.feed_forward_hidden = d_model * 4
 
-        # embedding for BERT, sum of positional, segment, token embeddings
-        self.embedding = BERTEmbedding(vocab_size=vocab_size, embed_size=d_model)
+        # embedding for BERT, sum of positional, token embeddings
+        self.embedding = BERTEmbedding(vocab_size=vocab_size, embed_size=d_model, seq_len=MAX_LEN, dropout=dropout)
 
         # multi-layers transformer blocks, deep network
         self.encoder_blocks = torch.nn.ModuleList(
             [EncoderLayer(d_model, heads, d_model * 4, dropout) for _ in range(n_layers)])
 
-    def forward(self, x, segment_info):
+    def forward(self, x):
         # attention masking for padded token
         # (batch_size, 1, seq_len, seq_len)
         mask = (x > 0).unsqueeze(1).repeat(1, x.size(1), 1).unsqueeze(1)
 
         # embedding the indexed sequence to sequence of vectors
-        x = self.embedding(x, segment_info)
+        x = self.embedding(x)
 
         # running over multiple transformer blocks
         for encoder in self.encoder_blocks:
@@ -438,12 +426,11 @@ class BERTLM(torch.nn.Module):
 
         super().__init__()
         self.bert = bert
-        self.next_sentence = NextSentencePrediction(self.bert.d_model)
         self.mask_lm = MaskedLanguageModel(self.bert.d_model, vocab_size)
 
-    def forward(self, x, segment_label):
-        x = self.bert(x, segment_label)
-        return self.next_sentence(x), self.mask_lm(x)                
+    def forward(self, x):
+        x = self.bert(x)
+        return self.mask_lm(x)                
     
 class ScheduledOptim():
     '''A simple wrapper class for learning rate scheduling'''
@@ -493,7 +480,8 @@ class BERTTrainer:
         ):
 
         self.device = device
-        self.model = model
+        print(f"Device: {self.device} *** *** *** *** ***")
+        self.model = model.to(device)
         self.train_data = train_dataloader
         self.test_data = test_dataloader
 
@@ -506,7 +494,8 @@ class BERTTrainer:
         # Using Negative Log Likelihood Loss function for predicting the masked_token
         self.criterion = torch.nn.NLLLoss(ignore_index=0)
         self.log_freq = log_freq
-        print("Total Parameters:", sum([p.nelement() for p in self.model.parameters()]))
+        total_parameters = sum([p.nelement() for p in self.model.parameters()])
+        print(f"Total Parameters: {total_parameters:,}")
     
     def train(self, epoch):
         self.iteration(epoch, self.train_data)
@@ -519,7 +508,9 @@ class BERTTrainer:
         avg_loss = 0.0
         total_correct = 0
         total_element = 0
-        
+
+        early_stopping = EarlyStopping(self.model, './', patience=7, verbose=True, delta=0.01)
+
         mode = "train" if train else "test"
 
         # progress bar
@@ -531,23 +522,20 @@ class BERTTrainer:
         )
 
         for i, data in data_iter:
-
             # 0. batch_data will be sent into the device(GPU or cpu)
             data = {key: value.to(self.device) for key, value in data.items()}
 
             # 1. forward the next_sentence_prediction and masked_lm model
-            next_sent_output, mask_lm_output = self.model.forward(data["bert_input"], data["segment_label"])
-
-            # 2-1. NLL(negative log likelihood) loss of is_next classification result
-            next_loss = self.criterion(next_sent_output, data["is_next"])
+            mask_lm_output = self.model(data["bert_input"])
 
             # 2-2. NLLLoss of predicting masked token word
             # transpose to (m, vocab_size, seq_len) vs (m, seq_len)
             # criterion(mask_lm_output.view(-1, mask_lm_output.size(-1)), data["bert_label"].view(-1))
-            mask_loss = self.criterion(mask_lm_output.transpose(1, 2), data["bert_label"])
-
-            # 2-3. Adding next_loss and mask_loss : 3.4 Pre-training Procedure
-            loss = next_loss + mask_loss
+            loss = self.criterion(mask_lm_output.transpose(1, 2), data["bert_label"])
+            early_stopping(loss)
+            if early_stopping.stop_flag:
+                print("Early Stopping *** *** *** *** *** *** ***")
+                break
 
             # 3. backward and optimization only in train
             if train:
@@ -556,16 +544,12 @@ class BERTTrainer:
                 self.optim_schedule.step_and_update_lr()
 
             # next sentence prediction accuracy
-            correct = next_sent_output.argmax(dim=-1).eq(data["is_next"]).sum().item()
             avg_loss += loss.item()
-            total_correct += correct
-            total_element += data["is_next"].nelement()
 
             post_fix = {
                 "epoch": epoch,
                 "iter": i,
                 "avg_loss": avg_loss / (i + 1),
-                "avg_acc": total_correct / total_element * 100,
                 "loss": loss.item()
             }
 
@@ -573,29 +557,28 @@ class BERTTrainer:
                 data_iter.write(str(post_fix))
         print(
             f"EP{epoch}, {mode}: \
-            avg_loss={avg_loss / len(data_iter)}, \
-            total_acc={total_correct * 100.0 / total_element}"
+            avg_loss={avg_loss / len(data_iter)}"
         ) 
 
 
 '''test run'''
 
 train_data = BERTDataset(
-   pairs, seq_len=MAX_LEN, tokenizer=tokenizer)
+   lines, seq_len=MAX_LEN, tokenizer=tokenizer)
 
 train_loader = DataLoader(
    train_data, batch_size=32, shuffle=True, pin_memory=True)
 
 bert_model = BERT(
   vocab_size=len(tokenizer.vocab),
-  d_model=768,
+  d_model=192, # 768,
   n_layers=2,
-  heads=12,
+  heads=6, # 12,
   dropout=0.1
 )
 
 bert_lm = BERTLM(bert_model, len(tokenizer.vocab))
-bert_trainer = BERTTrainer(bert_lm, train_loader, device='cpu')
+bert_trainer = BERTTrainer(bert_lm, train_loader, device=device)
 epochs = 20
 
 for epoch in range(epochs):
