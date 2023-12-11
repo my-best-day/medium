77d4fc29eb820d62b4e23f111cf1ebbaa8a09750
Ron Hirsch <ronhirsch@gmail.com>
2023-12-11 18:40:08 -0500

    dev

--- src/morph_data.py
+++ src/morph_data.py
@@ -1,3 +1,5 @@
+import os
+import glob
 import gzip
 import torch
 import random
@@ -13,7 +15,11 @@ def morph_data(in_path):
         if in_path.endswith(ext):
             out_path = in_path[:-len(ext)]
             break
-    out_path = out_path + '.msgpack'
+    out_path = out_path + '.msgpack.gz'
+    if os.path.exists(out_path):
+        print(f"Skipping {in_path} -> {out_path}")
+        return
+    print(f"Converting {in_path} -> {out_path}")
     
     timer = Timer("load precached dataset")
     opener = gzip.open if in_path.endswith('.gz') else open
@@ -27,17 +33,17 @@ def morph_data(in_path):
         lists.append(record['bert_label'].tolist())
     timer.print("converted to lists", True)
     
-    with open(out_path, 'wb') as file:
-        packed_data = msgpack.packb(lists)
-        file.write(packed_data)
-    timer.print("wrote msgpack", True)
-
-    with gzip.open(f"{out_path}.gz", "wb") as file:
+    # with gzip.open(f"{out_path}.gz", "wb") as file:
+    with gzip.open(out_path, "wb") as file:
         packed_data = msgpack.packb(lists)
         file.write(packed_data)
     timer.print("wrote msgpack.gz", True)
 
-morph_data('./datasets/train_data_177.pkl.gz')
-morph_data('./datasets/train_data_12.pkl')
+# morph_data('./datasets/train_data_177.pkl.gz')
+# morph_data('./datasets/train_data_12.pkl')
+
+files = glob.glob('./datasets/test_data_*.pkl.gz')
+for file in files:
+    morph_data(file)
 
 
