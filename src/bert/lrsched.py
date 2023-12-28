5bd7a43d3e7f2cd0c18598dfc94cc7824fc4e08c
Ron Hirsch <ronhirsch@gmail.com>
2023-12-28 21:11:52 +0000

    refined lr sched

--- src/bert/lrsched.py
+++ src/bert/lrsched.py
@@ -1,11 +1,18 @@
+import logging
+
 class LRScheduler:
     def __init__(self, optimizer):
         self.optimizer = optimizer
         self.lr = 0.00025
-        self.factor = 0.8
+        self.factor = 5.0 / 6.0
 
     def step(self, epoch):
-        self.lr *= self.factor
+        if epoch < 5:
+            self.lr /= self.factor
+        else:
+            self.lr *= self.factor
+
         for param_group in self.optimizer.param_groups:
             param_group['lr'] = self.lr
-        print(f'Epoch {epoch} learning rate: {self.lr}')
+
+        logging.info(f'Epoch {epoch} learning rate: {self.lr}')
