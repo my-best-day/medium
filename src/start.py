1ee82d3d57a2dbad5a94f8f8ef879e6689fe1dd5
Ron Hirsch <ronhirsch@gmail.com>
2024-01-04 09:23:06 -0500

    logging=info

--- src/start.py
+++ src/start.py
@@ -316,12 +316,12 @@ def config_logging(config):
     configure the base logger, prints to console and file
     """
     logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
 
     # on the console, skip the date. log time, level, and message
     console_formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s', datefmt='%H:%M:%S')
     console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.DEBUG)
+    console_handler.setLevel(logging.INFO)
     console_handler.setFormatter(console_formatter)
     logger.addHandler(console_handler)
 
