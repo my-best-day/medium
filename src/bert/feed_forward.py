3e140c6852366dabf8e4d0a3e55d0d3ee9bbe30f
Ron Hirsch <ronhirsch@gmail.com>
2024-01-09 13:15:36 -0500

    de-nesting calls

--- src/bert/feed_forward.py
+++ src/bert/feed_forward.py
@@ -5,13 +5,15 @@ class FeedForward(torch.nn.Module):
 
     def __init__(self, d_model, middle_dim=2048, dropout=0.1):
         super(FeedForward, self).__init__()
-        
+
         self.fc1 = torch.nn.Linear(d_model, middle_dim)
         self.fc2 = torch.nn.Linear(middle_dim, d_model)
         self.dropout = torch.nn.Dropout(dropout)
         self.activation = torch.nn.GELU()
 
     def forward(self, x):
-        out = self.activation(self.fc1(x))
-        out = self.fc2(self.dropout(out))
-        return out    
+        out = self.fc1(x)
+        out = self.activation(out)
+        out = self.dropout(out)
+        out = self.fc2(out)
+        return out
