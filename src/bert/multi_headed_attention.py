ccee9ed368436413273645ace6abedc1b96393a9
Ron Hirsch <ronhirsch@gmail.com>
2024-08-02 13:46:48 -0400

    dropout is conditional to self.training; PEP8

--- src/bert/multi_headed_attention.py
+++ src/bert/multi_headed_attention.py
@@ -4,8 +4,7 @@ import torch
 import logging
 
 use_flash = True
-is_gpt = True
-is_bert = False
+is_gpt = False
 
 
 ### attention layers
@@ -34,7 +33,8 @@ class MultiHeadedAttention(torch.nn.Module):
         if not self.flash and self.causal:
             # print("WARNING: using slow attention. Flash Attention requires PyTorch >= 2.0")
             # causal mask to ensure that attention is only applied to the left in the input sequence
-            self.register_buffer("bias", torch.tril(torch.ones(max_len, max_len)).view(1, 1, max_len, max_len))
+            self.register_buffer("bias", torch.tril(
+                torch.ones(max_len, max_len)).view(1, 1, max_len, max_len))
 
     def forward(self, query, key, value, mask):
         """
@@ -46,24 +46,25 @@ class MultiHeadedAttention(torch.nn.Module):
         key = self.key(key)
         value = self.value(value)
 
-        # (batch_size, max_len, d_model) --> (batch_size, max_len, h, d_k) --> (batch_size, h, max_len, d_k)
+        # (batch_size, max_len, d_model) --> (batch_size, max_len, h, d_k) -->
+        # (batch_size, h, max_len, d_k)
         query = query.view(query.shape[0], -1, self.heads, self.d_k).permute(0, 2, 1, 3)
         key = key.view(key.shape[0], -1, self.heads, self.d_k).permute(0, 2, 1, 3)
         value = value.view(value.shape[0], -1, self.heads, self.d_k).permute(0, 2, 1, 3)
 
         if self.flash:
-            # TODO: dropout only if training
-            training = True
-            dropout = self.dropout_value if training else 0
-            context = torch.nn.functional.scaled_dot_product_attention(query, key, value, attn_mask=None, dropout_p=dropout, is_causal=self.causal)
+            # need to set dropout manually when using flash attention
+            dropout = self.dropout_value if self.training else 0
+            context = torch.nn.functional.scaled_dot_product_attention(
+                query, key, value, attn_mask=None, dropout_p=dropout, is_causal=self.causal)
         else:
-            # (batch_size, h, max_len, d_k) matmul (batch_size, h, d_k, max_len) --> (batch_size, h, max_len, max_len)
+            # (batch_size, h, max_len, d_k) matmul (batch_size, h, d_k, max_len) -->
+            #      (batch_size, h, max_len, max_len)
             scores = torch.matmul(query, key.permute(0, 1, 3, 2)) / math.sqrt(query.size(-1))
 
             if is_gpt and not self.flash:
-                scores = scores.masked_fill(self.bias[:, :, :self.max_len, :self.max_len] == 0, float('-inf'))
-                # done below
-                # scores = torch.nn.functional.softmax(scores, dim=-1)
+                scores = scores.masked_fill(
+                    self.bias[:, :, :self.max_len, :self.max_len] == 0, float('-inf'))
 
             # fill 0 mask with super small number so it wont affect the softmax weight
             # (batch_size, h, max_len, max_len)
@@ -75,11 +76,16 @@ class MultiHeadedAttention(torch.nn.Module):
             weights = torch.nn.functional.softmax(scores, dim=-1)
             weights = self.dropout(weights)
 
-            # (batch_size, h, max_len, max_len) matmul (batch_size, h, max_len, d_k) --> (batch_size, h, max_len, d_k)
+            # (batch_size, h, max_len, max_len) matmul (batch_size, h, max_len, d_k) -->
+            #      (batch_size, h, max_len, d_k)
             context = torch.matmul(weights, value)
 
-        # (batch_size, h, max_len, d_k) --> (batch_size, max_len, h, d_k) --> (batch_size, max_len, d_model)
-        context = context.permute(0, 2, 1, 3).contiguous().view(context.shape[0], -1, self.heads * self.d_k)
+        # (batch_size, h, max_len, d_k) --> (batch_size, max_len, h, d_k) -->
+        #      (batch_size, max_len, d_model)
+        context = context \
+            .permute(0, 2, 1, 3) \
+            .contiguous() \
+            .view(context.shape[0], -1, self.heads * self.d_k)
 
         # (batch_size, max_len, d_model)
         projected = self.output_linear(context)
