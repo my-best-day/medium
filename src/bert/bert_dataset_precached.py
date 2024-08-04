6f8ae9ec4b2c4d93635483f721d09f810a71695a
Ron Hirsch <ronhirsch@gmail.com>
2024-08-04 13:57:44 -0400

    removed deprecated code

--- src/bert/bert_dataset_precached.py
+++ src/bert/bert_dataset_precached.py
@@ -1,10 +1,8 @@
 """
 Dataset for pre-cached BERT inputs and labels.
 """
-import re
 import gzip
 import torch
-import pickle
 import msgpack
 from torch.utils.data import Dataset
 
@@ -20,15 +18,8 @@ class BERTDatasetPrecached(Dataset):
         self._percentage = percentage
         opener = gzip.open if path.endswith('.gz') else open
         with opener(path, 'rb') as f:
-            if re.search(r'\.pkl(\.gz)?$', path):
-                self._type = 0
-                self.cached_data = pickle.load(f)
-            elif re.search(r'\.msgpack(\.gz)?$', path):
-                self._type = 1
-                packed_data = f.read()
-                self.cached_data = msgpack.unpackb(packed_data, raw=False)
-            else:
-                raise ValueError(f"Unknown file type: {path}")
+            packed_data = f.read()
+            self.cached_data = msgpack.unpackb(packed_data, raw=False)
         if percentage < 1.0:
             self.cached_data = self.cached_data[:int(len(self.cached_data) * percentage)]
         # logging.info(timer.step(f"loaded precached dataset {path}"))
@@ -37,12 +28,7 @@ class BERTDatasetPrecached(Dataset):
         """
         Returns the number of items in the dataset.
         """
-        if self._type == 0:
-            result = len(self.cached_data)
-        elif self._type == 1:
-            result = len(self.cached_data) // 2
-        else:
-            raise ValueError(f"Unknown type: {self._type}")
+        result = len(self.cached_data) // 2
         if self._percentage < 1.0:
             result = int(result * self._percentage)
         return result
@@ -51,15 +37,10 @@ class BERTDatasetPrecached(Dataset):
         """
         Retrieves a pre-cached BERT input and label.
         """
-        if self._type == 0:
-            return self.cached_data[index]['bert_input'], self.cached_data[index]['bert_label']
-        elif self._type == 1:
-            l_sentence = self.cached_data[index * 2]
-            l_labels = self.cached_data[index * 2 + 1]
-            sentence = torch.tensor(l_sentence, dtype=torch.int64)
-            labels = torch.tensor(l_labels, dtype=torch.int64)
-            del l_sentence
-            del l_labels
-            return sentence, labels
-        else:
-            raise ValueError(f"Unknown type: {self._type}")
+        l_sentence = self.cached_data[index * 2]
+        l_labels = self.cached_data[index * 2 + 1]
+        sentence = torch.tensor(l_sentence, dtype=torch.int64)
+        labels = torch.tensor(l_labels, dtype=torch.int64)
+        del l_sentence
+        del l_labels
+        return sentence, labels
