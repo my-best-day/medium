cc5e4d70ae28e3878d39d1aecd70cea28d7d65fe
Ron Hirsch <ronhirsch@gmail.com>
2024-08-14 11:14:14 -0400

    fixed import

--- scripts/check_sentence_length.py
+++ scripts/check_sentence_length.py
@@ -1,7 +1,7 @@
 import glob
 import torch
-from bert.bert_mlm_dataset_precached import BertMlmDatasetPrecached
-from bert.ml.mlm.dump_sentences import describe
+from data.mlm.bert_mlm_dataset_precached import BertMlmDatasetPrecached
+from bert.lm.mlm.dump_sentences import describe
 from utils.timer import Timer
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
